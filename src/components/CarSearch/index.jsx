ce8d991f47a07fc7f1cfdb26bd71ed8b85357898
HarunaTech <hamodharuna@gmail.com>
2024-03-20 22:58:24 +0200

    Added a drop dowm menu for car search

--- src/components/CarSearch/index.jsx
+++ src/components/CarSearch/index.jsx
@@ -1,10 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Slider from 'react-slick';
 import "./style.css";
-import Powerfull from "../../assets/sounds/Powerfull.mp3";
-import Quiet from "../../assets/sounds/Quiet.mp3";
-import Smooth from "../../assets/sounds/Smooth.mp3";
-import V6 from "../../assets/sounds/V6.mp3";
 
 ///THIS IS THE INDIVIDUAL CAR DISPLAY CARD
 const CarCard = ({ car }) => {
@@ -15,74 +11,6 @@ const CarCard = ({ car }) => {
     slidesToShow: 4,
     slidesToScroll: 1,
   };
-
-  // Function to add the car to favourites :
-  const [isFavorite, setIsFavorite] = useState(false);
-  // const [audio] = useState(new Audio(engineSound)); //  audio source is set to the 'engineSound'
-
-  useEffect(() => {
-    // Check if the car is already in favorites when the component mounts
-    const existingFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
-    const isAlreadyFavorite = existingFavorites.some(favorite => favorite.id === car.id);
-    setIsFavorite(isAlreadyFavorite);
-  }, [car.id]);
-
-  const playEngineSound = (engineSoundName) => {
-    if (engineSoundName === "Powerful") {
-      const audio = new Audio(Powerfull);
-      audio.play();
-    }
-    else if (engineSoundName === "Smooth") {
-      const audio = new Audio(Smooth);
-      audio.play();
-    }
-    else if (engineSoundName === "Quiet") {
-      const audio = new Audio(Quiet);
-      audio.play();
-    }
-    else if (engineSoundName === "V6") {
-      const audio = new Audio(V6);
-      audio.play();
-    }   
-    
-    
-  };
-
-  
- 
-  // const addToFavorites = () => {
-  //   const existingFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
-  //   const isAlreadyFavorite = existingFavorites.some(favorite => favorite.id === car.id);
-
-  //   if (!isAlreadyFavorite) {
-  //     const updatedFavorites = [...existingFavorites, car];
-  //     localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
-  //     setIsFavorite(true);
-  //     console.log('Added to favorites:', car);
-  //   } else {
-  //     const updatedFavorites = existingFavorites.filter(favorite => favorite.id !== car.id);
-  //     localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
-  //     setIsFavorite(false);
-  //     console.log('Car is already in favorites:', car);
-  //   } 
-  // };
-  const addToFavorites = () => {
-    const existingFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
-    const isAlreadyFavorite = existingFavorites.some(favorite => favorite.id === car.id);
-  
-    if (!isAlreadyFavorite) {
-      const updatedFavorites = [...existingFavorites, car];
-      localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
-      setIsFavorite(true);
-      console.log('Added to favorites:', car);
-    } else {
-      const updatedFavorites = existingFavorites.filter(favorite => favorite.id !== car.id);
-      localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
-      setIsFavorite(false);
-      console.log('Car is already in favorites:', car);
-    }
-  };
-
   return (
     <div className="car-card">
       <img src={car.image1} alt={car.car_brand} />
@@ -94,10 +22,7 @@ const CarCard = ({ car }) => {
         <li>Engine Size: {car.engineSize}</li>
         <li>Engine Sound: {car.engineSound}</li>
       </ul>
-      <button onClick={addToFavorites} className="custom-button">
-        {isFavorite ? 'Saved \u2665' : 'Add to Favorites \u2764'}
-      </button>
-      <button onClick={()=>playEngineSound(car.engineSound)} className="engine-button">&#128362;</button>
+      <button>Add to Favourites</button>
     </div>
   );
 };
@@ -165,25 +90,24 @@ const CarList = ({ cars }) => {
 ///THIS DEFINES THE SEARCH. ALSO HANDLERS TO AVOID ANY ERRORS RESULTING FROM CASE SENSITIVITY
 const CarSearch = ({ data }) => {
   const [type, setType] = useState("");
-  const [brand, setBrand] = useState("");
   const [location, setLocation] = useState("");
   const [filteredCars, setFilteredCars] = useState(data);
+
+  ///THIS CREATES AN ARRAY FROM THE CAR DATA TO INCLUDE ALL LOCATIONS IN THE DATA. BETTER THAN HARDCODING IN THE VALUES AS THEY MAY CHANGE
+  const locations = Array.from(new Set(data.map((car) => car.location)));
+
+  ///THIS CREATES AN ARRAY FROM THE CAR DATA TO INCLUDE ALL CAR TYPES IN THE DATA.
+  const types = Array.from(new Set(data.map((car) => car.type)));
 
   const handleSearch = () => {
     const filtered = data.filter((car) => {
       const typeMatch = car.type.toLowerCase().includes(type.toLowerCase());
-      // Added brand search button to search car by brand name
-      const brandMatch = car.car_brand.toLowerCase().includes(brand.toLowerCase()); 
       const locationMatch = car.location
         .toLowerCase()
         .includes(location.toLowerCase());
-      return typeMatch && locationMatch && brandMatch; 
+      return typeMatch && locationMatch;
     });
     setFilteredCars(filtered);
-    // Clears search input 
-    setType("");
-    setBrand("");
-    setLocation("");
   };
 
   ///THIS IS THE SEARCH FORM. WE SHOULD CHANGE type="text" TO LIST POPULATED BY THE AVAILABLE OPTIONS
@@ -192,25 +116,37 @@ const CarSearch = ({ data }) => {
     <div>
       <h1 className="heading">Search for Cars</h1>
       <div>
-        <input
+        {/*THIS WAS THE REGULAR TEXT INPUT... NOW REPLACED BY THE <select> below....:
+        /* <input
           type="text"
           placeholder="Enter car type"
           value={type}
           onChange={(e) => setType(e.target.value)}
-        />
-         {/* Input for car brand search */}
-                <input
-          type="text"
-          placeholder="Enter car brand name"
-          value={brand}
-          onChange={(e) => setBrand(e.target.value)}
-        />
-        <input
+        /> */}
+        <select value={type} onChange={(e) => setType(e.target.value)}>
+          <option value="">Select type</option>
+          {types.map((loc, index) => (
+            <option key={index} value={loc}>
+              {loc}
+            </option>
+          ))}
+        </select>
+
+        {/* <input
           type="text"
           placeholder="Enter location"
           value={location}
           onChange={(e) => setLocation(e.target.value)}
-        />
+        /> */}
+        <select value={location} onChange={(e) => setLocation(e.target.value)}>
+          <option value="">Select location</option>
+          {locations.map((loc, index) => (
+            <option key={index} value={loc}>
+              {loc}
+            </option>
+          ))}
+        </select>
+
         <button onClick={handleSearch}>Search</button>
       </div>
       <div className="car-list">
@@ -222,5 +158,7 @@ const CarSearch = ({ data }) => {
     </div>
   );
 };
+
+
 
 export default CarSearch;
